ba1b2a73a950594e444b77ea49b4af8628f51f6e
TEAM BLAZE <87557914+TEAM-BLAZ@users.noreply.github.com>
2021-11-27 13:24:48 +0530

    Update bLaZe.py

--- bLaZe.py
+++ bLaZe.py
@@ -71,10 +71,10 @@ async def mentionall(event):
     ):
       is_admin = True
   if not is_admin:
-    return await event.respond("__Only admins can mention all!__")
+    return await event.reply("__Only admins can mention all!__")
   
   if event.pattern_match.group(1) and event.is_reply:
-    return await event.respond("__Give me one argument!__")
+    return await event.reply("__Give me one argument!__")
   elif event.pattern_match.group(1):
     mode = "text_on_cmd"
     msg = event.pattern_match.group(1)
@@ -82,7 +82,7 @@ async def mentionall(event):
     mode = "text_on_reply"
     msg = await event.get_reply_message()
     if msg == None:
-        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+        return await event.reply("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
   else:
     return await event.reply("__Reply to a message or give me some text to mention others!__")
   
@@ -111,13 +111,13 @@ async def mentionall(event):
 @client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
   if not event.chat_id in spam_chats:
-    return await event.respond('__There is no proccess on going...__')
+    return await event.reply('__There is no proccess on going...__')
   else:
     try:
       spam_chats.remove(event.chat_id)
     except:
       pass
-    return await event.respond('__Stopped.__')
+    return await event.reply('__Stopped.__')
 
-print(">> XMART MENTIONBOT STARTED <<")
+print(">> BLAZE MENTIONBOT STARTED <<")
 client.run_until_disconnected()
